7cc6e3fbc235b691ed34b945f77242fcaf8ed3c5
unknown <ameralhomdy@gmail.com>
2019-11-12 11:42:27 -0700

    added base.html and twitter.py

--- TWITOFF/app.py
+++ TWITOFF/app.py
@@ -1,5 +1,6 @@
+from decouple import config
 from flask import Flask
-from .models import DB
+from .models import DB, User
 
 #now we make a app factory
 
@@ -7,13 +8,14 @@ def create_app():
     app = Flask(__name__)
 
     #add our config
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+    app.config['SQLALCHEMY_DATABASE_URI'] = config('DATABASE_URL')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     #now have the database know about the app
     DB.init_app(app)
 
     @app.route('/')
     def root():
-        return 'Welcome to Twitoff'
+        users = User.query.all()
+        return render_tempelate('base.html', title='Home', users=users)
     return app
-    
--- TWITOFF/models.py
+++ TWITOFF/models.py
@@ -9,8 +9,18 @@ class User(DB.Model):
     """Twitter users that we analyze"""
     id = DB.Column(DB.Integer, primary_key=True)
     name = DB.Column(DB.String(15), nullable=False)
+    newest_tweet_id = DB.Column(DB.BigInteger)
+
+    def __repr__(self):
+        return '<User {}>'.format(self.name)
+
 
 class Tweet(DB.Model):
     """The user's tweets from twitter"""
     id = DB.Column(DB.Integer, primary_key=True)
-    text = DB.Column(DB.Unicode(280))
+    text = DB.Column(DB.Unicode(300))
+    user_id = DB.Column(DB.Integer, DB.ForeignKey('user.id'), nullable=False)
+    user = DB.relationship('User', backref=DB.backref('tweets', lazy=True))
+
+    def __repr__(self):
+        return '<Tweet {}>'.format(self.text)
